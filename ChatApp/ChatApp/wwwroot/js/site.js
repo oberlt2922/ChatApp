432c20e90de8e705258551f979a154237c5886e3
oberlt2922 <53795023+oberlt2922@users.noreply.github.com>
2021-08-25 15:53:25 -0400

    display chatroom
    When a chatroom li element is clicked, the active chatroom is changed.  In the chatroom panel, the chatroom name is displayed as well as the members and messages count.  Messages should also be displayed with the correct classes and styling, but this cannot be tested until the website is able to create and send messages.

--- ChatApp/ChatApp/wwwroot/js/site.js
+++ ChatApp/ChatApp/wwwroot/js/site.js
@@ -1,16 +1,41 @@
 ﻿var userId;
 
 $(document).ready(function () {
+    //display chatroom
+    function displayChatroom(chatroom) {
+        $('.chat_chatroom_name').text(chatroom.chatroomName);
+        $('.message_count').text(chatroom.messages.length + ' Messages');
+        $('.members_count').text(chatroom.members.length + ' Members');
+        $('.msg_card_body').empty();
+        $.each(chatroom.messages, function (index, message) {
+            var div = '<div class="d-flex mb-4"></div>';
+            var msgContainer = '<div></div>';
+            if (message.Sender.id == userId) {
+                div.addClass('justify-content-end');
+                msgContainer.addClass('msg_cotainer_send');
+            }
+            else {
+                div.addClass('justify-content-start');
+                msgContainer.addClass('msg_cotainer');
+            }
+            $(msgContainer).html(message.text + '<span class="msg_time">' + message.Sender.userName + ' ' + message.sent + '</span>')
+            div.append(msgContainer);
+            $('.msg_card_body').append(div);
+        });
+        //add hidden input with chatroom id to send message form
+    }
+
+
     //AJAX FUNCTIONS
-    //get chatroom ajax function
     function getChatroom(id) {
         $.ajax({
             type: 'POST',
             url: '../Home/GetChatroom',
             data: { 'chatroomId': id },
             dataType: 'json'
-        }).done(function (json) {
-            console.log(json);
+        }).done(function (result) {
+            console.log(result);
+            displayChatroom(result);
         });
     }
 
@@ -31,9 +56,12 @@ $(document).ready(function () {
         $(this).parent().remove();
     });
 
+    //calls getchatroom with calls display chatroom
     $('ui.contacts').on('click', 'li.chatroomListItem', function (event) {
         var chatroomId = $(this).find('.chatroom_id');
         userId = $('.user_id');
+        $('.active').removeClass('active');
+        $(this).addClass('active');
         getChatroom(chatroomId.val());
     });
 });
